e16a2cbf13c1427709facf7f8d0d3cdf02b8e76e
Raghav-56 <142162663+Raghav-56@users.noreply.github.com>
2024-06-12 15:35:41 +0530

    .sx

---
+++ begin/list.hs
@@ -0,0 +1,10 @@
+lostNumbers = [4,8,15,16,23,42]
+ln = lostNumbers
+h = ['h','e','l','l','o']
+w = ['w','o','r','l','d']
+r = "raghav"
+a = [1,2,3]
+b = [4,5,6]
+
+{-ghci> h++' ':r
+"hello raghav"-}
