af7f53e2c5c23425671688a69083c1205aefa70a
Raghav-56 <142162663+Raghav-56@users.noreply.github.com>
2024-06-13 09:49:12 +0530

    .

--- begin/func.hs
+++ begin/func.hs
@@ -10,3 +10,11 @@ doubleSmallNumber x = if x > 100
 
 
 doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+
+
+-- && means a boolean and, || means a boolean or. not negates a True or a False.
+
+{-
+infix function: we call it by sandwiching it between arguments (usually numbers).
+prefix functions: In most imperative languages functions are called by writing the function name and then writing its parameters in parentheses, usually separated by commas. In Haskell, functions are called by writing the function name, a space and then the parameters, separated by spaces.
+-}
