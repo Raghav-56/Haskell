9d9354bd87948410a32666dfcb30170221c95409
Raghav-56 <raghavrgupta56@gmail.com>
2024-06-14 09:59:29 +0530

    Remove .pdf and .ps1 files from tracking

---
+++ begin/tuples.hs
@@ -0,0 +1,5 @@
+--used when you know exactly how many and of what type values you want to combine
+--Type of Tuples  depends on how many components it has and the types of the components.
+
+vectors = [(0,0),(1,0),(1,1),(0,1)]
+name&age = [("raghav","gupta",18),("ram","sharma",18)]
